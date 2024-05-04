d80041d9108cdcf5fdc76cd81e1dde997379bd16
raje <rpancholi522@gmail.com>
2024-05-04 13:47:07 +0530

    correction to cipher errors

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,4 +1,4 @@
-public class CeaserCipher{
+public class CaesarCipher{
     protected char[] encoder = new char[26];
     protected char[] decoder = new char[26];
 
@@ -11,6 +11,9 @@ public class CeaserCipher{
     public String encrypt(String message){
             return transform(message, encoder);
     }
+    public String decrypt(String secret){
+        return transform(secret, decoder);
+    }
     private String transform(String original, char[] code){
         char[] msg = original.toCharArray();
         for(int k=0; k<msg.length; k++){
@@ -18,10 +21,17 @@ public class CeaserCipher{
                 int j = msg[k]-'A';
                 msg[k] = code[j];
             }
-            return new String(msg);
         }
+        return new String(msg);
     }
     public static void main(String[] args){
-        // code here
+        CaesarCipher cipher = new CaesarCipher(8);
+        System.out.println("Encryption code = "+ new String(cipher.encoder));
+        System.out.println("Decryption code = "+ new String(cipher.decoder));
+        String message = "My name is Rajendra Pancholi";
+        String coded = cipher.encrypt(message);
+        System.out.println("Secret: "+ coded);
+        String answer = cipher.decrypt(coded);
+        System.out.print("Message: "+answer);
     }
 }
