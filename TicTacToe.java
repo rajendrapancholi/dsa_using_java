0f0fb17b1bac5982552ffc1cd7ec8a346be34d9e
raje <rpancholi522@gmail.com>
2024-05-04 15:04:05 +0530

    create a tictactoe game and initialize variable, and main function.

---
+++ TicTacToe.java
@@ -0,0 +1,8 @@
+public class TicTacToe{
+    public static final int X = 1, O = -1;
+    public static final int EMPTY = 0;
+    
+    public static void main(String[] args){
+        // code here
+    }
+}
